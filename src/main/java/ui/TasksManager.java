180facb053de25ef9b6e43d680eda72c31c7dd69
Kostiantyn Perets <kostyaperec3@gmail.com>
2020-04-13 20:21:54 +0300

    Refactor after comments

--- src/main/java/ui/TasksManager.java
+++ src/main/java/ui/TasksManager.java
@@ -1,18 +1,17 @@
 package ui;
 
-import constants.Constants;
 import entity.Task;
 
 
-import java.io.*;
+import commands.TaskManagerCommands;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class TasksManager {
     private static LocalDateTime localDateTime = LocalDateTime.now();
     private static String fileName;
     public List<Task> tasks = new ArrayList<>();
+    TaskManagerCommands command = new TaskManagerCommands();
 
     public void menu() {
         System.out.println("Hello, dear User!");
@@ -20,7 +19,7 @@ public class TasksManager {
         do {
             do {
                 System.out.println("What do you want to do?(You can input \"h\" for help) ");
-                input = scanConsoleInput();
+                input = TaskManagerCommands.scanConsoleInput();
             } while (input.length() == 0);
 
             chooseAction(input.charAt(0));
@@ -30,10 +29,10 @@ public class TasksManager {
     private void chooseAction(char input) {
         switch (input) {
             case ('c'):
-                createTask();
+                command.createTask();
                 break;
             case ('r'):
-                removeTask();
+                command.removeTask();
                 break;
             case ('h'):
                 System.out.println("Operations available to you: \n" +
@@ -43,7 +42,7 @@ public class TasksManager {
                         "q - quit;");
                 break;
             case ('s'):
-                showTask();
+                command.showTask();
                 break;
             case ('q'):
                 System.exit(0);
@@ -53,96 +52,5 @@ public class TasksManager {
         }
     }
 
-    private void showTask() {
-        tasks = deserializeTasks();
-        System.out.println(tasks);
-        System.out.println("show");
-    }
 
-    private void removeTask() {
-        System.out.println("remove");
-    }
-
-    private void createTask() {
-        tasks.add(new Task("EXAMPLE", "to do"));
-        tasks.add(new Task("to do", "EXAMPLE"));
-        serializeTasks(tasks);
-        System.out.println("create");
-    }
-
-    public static String scanConsoleInput() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
-
-    private static void serializeTasks(List<Task> tasks) {
-        setFileName();
-        File file = createNewFile(fileName);
-        assert file != null;
-        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))){
-            out.writeObject(tasks);
-        }
-        catch (FileNotFoundException e){
-            createNewFile(fileName);
-            //log error message
-        }
-        catch (IOException e){
-            //log error message
-        }
-    }
-
-    private static List<Task> deserializeTasks(){
-        File file = new File(Constants.PATH);
-        File[] files = file.listFiles();
-        assert files != null;
-        File temp = getLastModifiedFile(files);
-
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(temp))){
-            return (ArrayList) in.readObject();
-        }
-        catch (FileNotFoundException e){
-            if (fileName == null) {
-                setFileName();
-            }
-            createNewFile(fileName);
-            //log error message
-        }
-        catch (IOException e){
-            e.printStackTrace();
-            //log error message
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static File createNewFile(String fileName){
-        File file = new File(Constants.PATH);
-        if(!file.isDirectory()) {
-            file.mkdir();
-        }
-        file = new File( Constants.PATH + localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE) +
-                "," + localDateTime.getHour() + "-" + localDateTime.getMinute() + "," + fileName + ".bin");
-        try {
-            file.createNewFile();
-            return file;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static File getLastModifiedFile(File[] files){
-        if(files.length > 0){
-            Arrays.sort(files, Comparator.comparingLong(File::lastModified));
-        }
-        return files[files.length - 1];
-    }
-
-    private static void setFileName(){
-        System.out.println("Enter file name");
-        //logger message about enter file name
-        fileName = scanConsoleInput();
-    }
 }
