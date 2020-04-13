a7f1fccc4896fa761d61fe63a08f8374d73f0d3f
QA engineer <kotarakh@gmail.com>
2020-04-13 22:02:04 +0300

    format Task.toString() (not done)

--- src/main/java/entity/Task.java
+++ src/main/java/entity/Task.java
@@ -1,17 +1,20 @@
 package entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Task implements Serializable{
     private String title;
 
     private String description;
-   // private LocalDateTime creationDate;
+    private LocalDateTime creationDate;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public Task(String title, String description){ //String deadline) {
         this.title = title;
         this.description = description;
-       //this.creationDate = LocalDateTime.now();
+        this.creationDate = LocalDateTime.now();
     }
 
     public String getTitle() {
@@ -32,9 +35,9 @@ public class Task implements Serializable{
 
     @Override
     public String toString() {
-        return "Task{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
+        return  "title: " + title + '\'' +
+                "(" + description + ")\'" +
+                ", created='" + creationDate.format(formatter) + '\'' +
                 '}';
     }
 }
